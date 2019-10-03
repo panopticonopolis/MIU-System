da05bf5081d017f6eda5e8a8853f18f9922dd24c
panopticonopolis <46457754+panopticonopolis@users.noreply.github.com>
2019-10-03 15:38:25 -0400

    Update MIU.py

--- MIU.py
+++ MIU.py
@@ -1,9 +1,9 @@
 # MIU-System
 # A simple program that allows a user to build theorems in the MIU-System, as proposed in GEB, pp33-5.
 
-def MIU(axiom):
+def MIU(theorem):
     while True:
-        print("\nAxiom is currently", axiom)
+        print("\nTheorem is currently", theorem)
         choice = input("\nPlease enter rule number, 'r' to see the rules again, or ENTER to quit: ")
         if not choice:
             print('\nGoodbye!\n')
@@ -11,35 +11,28 @@ def MIU(axiom):
         elif choice == 'r' or choice == 'R':
             rules()
         elif choice == '1': 
-            if axiom[-1] == 'I':
-                axiom += 'U'
+            if theorem[-1] == 'I':
+                theorem += 'U'
             else:
-                print("\nCan't apply rule 1 with axiom", axiom)
+                print("\nCan't apply rule 1 with theorem", theorem)
         elif choice == '2':
-            axiom += axiom[1:]
+            theorem += theorem[1:]
         elif choice == '3':
-            if 'III' in axiom:
-                # print([axiom[i] for i in axiom if 'III' in axiom])
-                l = []
-                for i in range(len(axiom)):
-                    if i < (len(axiom) - 2) and 'III' in axiom[i:i+3]:
-                        l.append(i)
+            if 'III' in theorem:
+                l = [i for i in range(len(theorem)) if i < (len(theorem) - 2) and 'III' in theorem[i:i+3]]
                 print("\nYou can replace III --> U at these points:", l)
                 x = int(input("Please enter a point: "))
-                axiom = axiom[:x] + 'U' + axiom[x+3:]
+                theorem = theorem[:x] + 'U' + theorem[x+3:]
             else:
-                print("\nCan't apply rule 3 with axiom", axiom)
+                print("\nCan't apply rule 3 with theorem", theorem)
         elif choice == '4':
-            if 'UU' in axiom:
-                l = []
-                for i in range(len(axiom)):
-                    if i < (len(axiom) - 1) and 'UU' in axiom[i:i+2]:
-                        l.append(i)
+            if 'UU' in theorem:
+                l = [i for i in range(len(theorem)) if i < (len(theorem) - 1) and 'UU' in theorem[i:i+2]]
                 print("\nYou can reduce UU --> U at these points:", l)
                 x = int(input("Please enter a point: "))
-                axiom = axiom[:x] + 'U' + axiom[x+2:]
+                theorem = theorem[:x] + 'U' + theorem[x+2:]
             else:
-                print("\nCan't apply rule 4 with axiom", axiom)
+                print("\nCan't apply rule 4 with theorem", theorem)
         else:
             print("\nI'm sorry, I didn't understand. Please try again.")
     
@@ -54,8 +47,6 @@ def main():
     axiom = 'MI'
 
     print('\nWelcome to the MIU System')
-    print("\nWe begin with an axiom of '" + axiom + "'.")
+    print("\nWe begin with an theorem of '" + axiom + "'.")
     rules()
     MIU(axiom)
-
-main()
